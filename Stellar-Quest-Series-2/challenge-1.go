51b6d506c23c7b97350907f2f0eac261d1807484
mtfallsVR <mtfallsVR@atemosta.com>
2021-01-02 19:44:27 -0500

    c1 and c2

---
+++ Stellar-Quest-Series-2/challenge-1.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"encoding/hex"
+	"fmt"
+	"log"
+
+	"github.com/stellar/go/clients/horizonclient"
+	"github.com/stellar/go/keypair"
+	"github.com/stellar/go/network"
+	"github.com/stellar/go/txnbuild"
+)
+
+func main-c1() {
+
+	// Use the default pubnet client
+	kp, _ := keypair.Parse("MASTER_ACCOUNT_SECRET_KEY")
+	client := horizonclient.DefaultTestNetClient
+	ar := horizonclient.AccountRequest{AccountID: kp.Address()}
+	sourceAccount, err := client.AccountDetail(ar)
+	// check(err)
+	if err != nil {
+		fmt.Println("Error 1")
+		log.Fatalln(err)
+		return
+	}
+
+	op := txnbuild.CreateAccount{
+		Destination: "SERIES_2_PUBLIC_KEY",
+		Amount:      "5000",
+	}
+
+	var txnMemo [32]byte
+	hexBytes, err := hex.DecodeString("e3366fcb087bdb2381b7069a19405b748da831c18145eba25654d1092e93ef37")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	copy(txnMemo[:], hexBytes)
+
+	tx, err := txnbuild.NewTransaction(
+		txnbuild.TransactionParams{
+			SourceAccount:        &sourceAccount,
+			IncrementSequenceNum: true,
+			Operations:           []txnbuild.Operation{&op},
+			BaseFee:              txnbuild.MinBaseFee,
+			Timebounds:           txnbuild.NewInfiniteTimeout(), // Use a real timeout in production!
+			Memo:                 txnbuild.MemoHash(txnMemo),
+			// Memo:                 txnbuild.MemoText(str),
+		},
+	)
+	// check(err)
+	if err != nil {
+		fmt.Println("Error 2")
+		fmt.Println(err)
+		return
+	}
+
+	tx, err = tx.Sign(network.TestNetworkPassphrase, kp.(*keypair.Full))
+	// check(err)
+	if err != nil {
+		fmt.Println("Error 3")
+		fmt.Println(err)
+		return
+	}
+
+	txe, err := tx.Base64()
+	// check(err)
+	if err != nil {
+		fmt.Println("Error 4")
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(txe)
+
+	// submit transaction
+	// resp, err := client.SubmitTransactionXDR(txe)
+	// if err != nil {
+	// 	fmt.Println("Failed to sent Transactions")
+	// 	fmt.Println(err)
+	// 	return
+	// }
+
+	// fmt.Println(resp)
+}
---
+++ Stellar-Quest-Series-2/challenge-2.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/stellar/go/clients/horizonclient"
+	"github.com/stellar/go/keypair"
+	"github.com/stellar/go/network"
+	"github.com/stellar/go/txnbuild"
+)
+
+func main-c2() {
+	client := horizonclient.DefaultTestNetClient
+
+	// Remember, these are just examples, so replace them with your own seeds.
+	issuerSeed := "MASTER_ACCOUNT_SECRET_KEY"
+	distributorSeed := "QUEST_SERIES_2_SECRET_KEY"
+
+	/*
+	 * We omit error checks here for brevity, but you should always check your
+	 * return values.
+	 */
+
+	// Keys for accounts to issue and distribute the new asset.
+	issuer, err := keypair.ParseFull(issuerSeed)
+	distributor, err := keypair.ParseFull(distributorSeed)
+
+	request := horizonclient.AccountRequest{AccountID: issuer.Address()}
+	issuerAccount, err := client.AccountDetail(request)
+
+	request = horizonclient.AccountRequest{AccountID: distributor.Address()}
+	distributorAccount, err := client.AccountDetail(request)
+
+	// Create an object to represent the new asset
+	astroCheems := txnbuild.CreditAsset{Code: "AstroCheems", Issuer: issuer.Address()}
+
+	// First, the receiving (distribution) account must trust the asset from the
+	// issuer.
+	op1 := txnbuild.ChangeTrust{
+		Line: astroCheems,
+		// Limit:         "454545",
+		// SourceAccount: &distributorAccount,
+	}
+
+	// Second, the issuing account actually sends a payment using the asset
+	op2 := txnbuild.Payment{
+		Destination:   distributor.Address(),
+		Asset:         astroCheems,
+		Amount:        "4545",
+		SourceAccount: &issuerAccount,
+	}
+
+	// Assemble Transaction
+	tx, err := txnbuild.NewTransaction(
+		txnbuild.TransactionParams{
+			SourceAccount:        &distributorAccount,
+			IncrementSequenceNum: true,
+			BaseFee:              txnbuild.MinBaseFee,
+			Timebounds:           txnbuild.NewInfiniteTimeout(),
+			Operations:           []txnbuild.Operation{&op1, &op2},
+		},
+	)
+
+	// check(err)
+	if err != nil {
+		fmt.Println("Error 2")
+		fmt.Println(err)
+		return
+	}
+
+	tx, err = tx.Sign(network.TestNetworkPassphrase, issuer, distributor)
+	// check(err)
+	if err != nil {
+		fmt.Println("Error 3")
+		fmt.Println(err)
+		return
+	}
+
+	txe, err := tx.Base64()
+	// check(err)
+	if err != nil {
+		fmt.Println("Error 4")
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(txe)
+
+	// signedTx, err = tx.Sign(network.TestNetworkPassphrase, issuer)
+	// resp, err = client.SubmitTransaction(signedTx)
+
+	// if err != nil {
+	// 	log.Fatal(err)
+	// } else {
+	// 	log.Printf("Pay: %s\n", resp.Hash)
+	// }
+}
